e24e0b854caa78a27567a7c7b1829126ac8b46b2
Mangenese <blakemangen20@gmail.com>
2020-01-20 18:23:24 -0700

    added a the firebase items on the app.js and linked the firebase into the html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,24 @@
+//on user input it grabs what the people type 
+// it will then append to the list on the top part
+// it will store the users input into firebase
+
+var firebaseConfig = {
+    apiKey: "AIzaSyB7mI7wkW0dXeBKJxWnmz5qLumRoZjGD7Q",
+    authDomain: "train-schedule-2f0f1.firebaseapp.com",
+    databaseURL: "https://train-schedule-2f0f1.firebaseio.com",
+    projectId: "train-schedule-2f0f1",
+    storageBucket: "train-schedule-2f0f1.appspot.com",
+    messagingSenderId: "746622201981",
+    appId: "1:746622201981:web:bcc00c108c06481ec58114",
+    measurementId: "G-ML0PBHPSH2"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+var database = firebase.database();
+
+//on click functiin 
 $("#add-train").on("click", function() {
     event.preventDefault();
     
